5fe3d19b83a6e7265d3df3c6df8547a1971a8068
leighmaj <51490479+leighmaj@users.noreply.github.com>
2020-03-09 10:47:19 -0500

    allows bowlers to play normal or with one strike and score correctly

--- BowlingLab/Program.cs
+++ BowlingLab/Program.cs
@@ -10,6 +10,70 @@ namespace BowlingLab
     {
         static void Main(string[] args)
         {
+            string[] playerNames = new string[2];
+
+
+
+            int[,] playerScores = new int[2, 11];
+
+            Console.Write("Please enter your name: ");
+            playerNames[0] = Console.ReadLine();
+
+            Console.Write("Please enter your name: ");
+            playerNames[1] = Console.ReadLine();
+
+            Random randonNumGen = new Random();
+            int pinsKnockedDown;
+            int score = 0;
+            int totalScore = 0;
+
+            for (int frames = 0; frames < 11; frames++)//number of frames
+            {
+
+
+                for (int player = 0; player < 2; player++)//player 1 and 2
+                {
+                    Console.WriteLine($"It's {playerNames[player]}'s turn! Hit ENTER to bowl!");
+                    Console.ReadLine();
+                    Console.WriteLine();
+
+
+                    pinsKnockedDown = randonNumGen.Next(10);
+                    Console.WriteLine($"You knocked down {pinsKnockedDown} pins, bowl again!");
+                    Console.ReadLine();
+                    score += pinsKnockedDown;
+
+                    if (pinsKnockedDown < 10)
+                    {
+                        pinsKnockedDown = randonNumGen.Next(10 - pinsKnockedDown);
+                        Console.WriteLine($"You knocked down {pinsKnockedDown} pins.");
+                        Console.ReadLine();
+                        score += pinsKnockedDown + playerScores[player, frames];
+                    }
+                    else if (pinsKnockedDown == 10)
+                    {
+                        Console.WriteLine("Congrats! You got a strike!");
+                        playerScores[player, frames + 1] = 10;
+                    }
+                    
+                    playerScores[player, frames] = score;
+
+                    for (int i = frames; i >= 0; i--)
+                    {
+                        totalScore += playerScores[player, i];
+                        Console.WriteLine(totalScore);
+                    }
+                    
+                    Console.WriteLine($"{playerNames[player]} total frame {frames + 1} score is {playerScores[player, frames]} and your total score is {totalScore}");
+                    score = 0;
+                    Console.WriteLine(totalScore);
+
+                    totalScore = 0;
+
+                }
+            }
+
+
         }
     }
 }
